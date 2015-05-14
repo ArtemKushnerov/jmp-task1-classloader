87845323acd6409be9aeecd1251c663c3f57cc7c
Artem K <kushnerovartem@gmail.com>
2015-05-14 16:47:46 +0300

    Add classloader itself.

---
+++ src/main/java/DiskClassloader.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * The class that intended to load other classes from specified directory into the memory.
+ *
+ * @author Artsiom Kushniarou
+ * @since May 13, 2015
+ */
+public class DiskClassLoader extends ClassLoader {
+
+    /**
+     * Path to the file on the disk.
+     */
+    private String path2Class;
+
+    public DiskClassLoader(String path2Class) {
+        this.path2Class = path2Class;
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        System.out.println("Loading in progress...");
+        Path path = Paths.get(path2Class);
+        byte[] classData = null;
+        try {
+            classData = Files.readAllBytes(path);
+        } catch (IOException e) {
+            System.out.println("Error: " + e );
+        }
+        Class<?> aClass = defineClass(name, classData, 0, classData.length);
+        System.out.println("Congrats! The class '" + aClass.getName()+ "' is successfully loaded from the disk.");
+        return aClass;
+    }
+
+    public String getPath2Class() {
+        return path2Class;
+    }
+
+    public void setPath2Class(String path2Class) {
+        this.path2Class = path2Class;
+    }
+
+}
